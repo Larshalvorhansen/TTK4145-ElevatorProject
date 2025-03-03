04723cd99ed06afc62d8a91873df2a5fa6b2bae4
erikstan <erik.a.standal@gmail.com>
2025-03-03 11:22:34 +0100

    assigner and lights

--- lights/lights.go
+++ lights/lights.go
@@ -2,23 +2,23 @@ package elevator
 
 import (
 	"Driver-go/config"
-	//"Driver-go/distributor"
+	"Driver-go/distributor"
 	"Driver-go/elevio"
 )
 
 // SetLights sets the lights for the elevator system based on the current state.
-func SetLights(cs distributor.CommonState, ElevatorID int) { // TODO: check if this is implemented in distributor
+func SetLights(common_state distributor.CommonState, ElevatorID int) { // TODO: check if this is implemented in distributor
 	// Set hall button lights
 	for f := 0; f < config.NumFloors; f++ {
 		for b := 0; b < 2; b++ { // Iterate over button types (up and down)
 			// Set the hall button light for each floor and button type
-			elevio.SetButtonLamp(elevio.ButtonType(b), f, cs.HallRequests[f][b])
+			elevio.SetButtonLamp(elevio.ButtonType(b), f, common_state.HallOrders[f][b])
 		}
 	}
 
 	// Set cab button lights
 	for f := 0; f < config.NumFloors; f++ {
 		// Set the cab button light for each floor
-		elevio.SetButtonLamp(elevio.BT_Cab, f, cs.States[ElevatorID].CabRequests[f])
+		elevio.SetButtonLamp(elevio.BT_Cab, f, common_state.ElevatorStates[ElevatorID].CabOrders[f])
 	}
 }
