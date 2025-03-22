9676768d38dde8d59bdf1c61ad7bb7cd75a753d7
Ferdinand Hermann <>
2025-03-22 14:23:54 +0100

    run.sh is good

--- run.sh
+++ run.sh
@@ -1,25 +1,7 @@
-# trap 'echo "Ignoring Ctrl+C...";' SIGINT 
-
-# while true; do
-#     echo "Building the project..."
-#     go build -o elevator_program main.go || { echo "Build failed. Retrying..."; sleep 1; continue; }
-
-#     echo "Starting elevator program..."
-#     ./elevator_program
-
-#     echo "Program crashed or terminal closed. Restarting in a new window..."
-    
-#     sleep 1 
-#     gnome-terminal -- bash -c "cd $(pwd); ./run.sh $ID; exec bash"
-#     exit 
-# done
-
-
-#!/bin/bash
-
-# Hvis ID ikke er oppgitt, spør brukeren (bruk default 0 hvis enter trykkes)
 if [ -z "$1" ]; then
-    read -p "Enter elevator ID (default 0): " inputID
+    echo -e "\nEnter elevator ID (default 0):"
+    read -p "> " inputID
+    # read -p "Enter elevator ID (default 0): " inputID
     if [ -z "$inputID" ]; then
         ID=0
     else
@@ -29,9 +11,10 @@ else
     ID=$1
 fi
 
-# Hvis PORT ikke er oppgitt, spør brukeren (bruk default 15657 hvis enter trykkes)
 if [ -z "$2" ]; then
-    read -p "Enter elevator port (default 15657): " inputPort
+    echo -e "Enter elevator port (default 15657):"
+    read -p "> " inputPort
+    #read -p "Enter elevator port (default 15657): " inputPort
     if [ -z "$inputPort" ]; then
         PORT=15657
     else
@@ -41,18 +24,18 @@ else
     PORT=$2
 fi
 
-trap 'echo "Ignoring Ctrl+C...";' SIGINT 
+trap 'echo -e "\nPressed Ctrl+C...";' SIGINT 
 
 while true; do
-    echo "Building the project..."
-    go build -o elevator_program main.go || { echo "Build failed. Retrying..."; sleep 1; continue; }
+    echo -e "\nBuilding the project..."
+    go build -o elevator_program main.go || { echo -e "\nBuild failed. Retrying..."; sleep 1; continue; }
 
-    echo "Starting elevator program with ID=$ID and PORT=$PORT..."
+    echo -e "\nStarting elevator program with ID=$ID and PORT=$PORT...\n"
     ./elevator_program -id=$ID -port=$PORT
 
-    echo "Program crashed or terminal closed. Restarting in a new window..."
+    echo -e "\nProgram crashed or terminal closed. Restarting in a new window...\n"
     sleep 1 
-    # Åpner nytt terminalvindu og sender med verdiene slik at prompten ikke vises igjen.
+    
     gnome-terminal -- bash -c "cd $(pwd); ./run.sh $ID $PORT; exec bash"
     exit 
 done
